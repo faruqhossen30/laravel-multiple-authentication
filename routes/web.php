e3a0590055f08bc3d9ab8568c69842daba92c349
unknown <faruqhossen30@gmail.com>
2021-07-10 15:28:38 +0600

    Multiple authtication done !

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AdminController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+Route::get('/1', [AdminController::class,'one']);
+Route::get('/2', [AdminController::class,'two']);
+Route::get('/3', [AdminController::class,'three']);
